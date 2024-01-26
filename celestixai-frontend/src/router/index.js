b1cc11dc08bac0c5c04f0711812445b5beb3b413
Khadim Hussain <khadim.hussain@elunic.net>
2024-01-26 15:30:41 +0500

    fix/remote unnecessary routes

--- celestixai-frontend/src/router/index.js
+++ celestixai-frontend/src/router/index.js
@@ -1,26 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LandingPage from '../components/LandingPage.vue';
-import Dashboard from '../views/Dashboard.vue';
-import TextCorpus from '../views/TextCorpus.vue';
-import LLMs from '../views/LLMs.vue';
-import LLMConstellation from '../views/LLMConstellation.vue';
-import Chat from '../views/Chat.vue';
-import Deployments from '../views/Deployments.vue';
 import Auth from '../components/Auth.vue'
 import MainApp from '../views/MainApp.vue';
 
 const routes = [
-  { path: '/landingpage', component: LandingPage },
-  { path: '/', redirect: '/landingpage' },
-  { path: '/:catchAll(.*)', redirect: '/landingpage' },
+  { path: '/', component: LandingPage },
+  { path: '/:catchAll(.*)', redirect: '/' },
   {path:  '/auth', component : Auth},
   { path: '/app', component: MainApp, meta: { requiresAuth: true } },
-  { path: '/dashboard', component: Dashboard, meta: { requiresAuth: true } },
-  { path: '/text-corpus', component: TextCorpus, meta: { requiresAuth: true } },
-  { path: '/llms', component: LLMs, meta: { requiresAuth: true } },
-  { path: '/llm-constellation', component: LLMConstellation, meta: { requiresAuth: true } },
-  { path: '/chat', component: Chat, meta: { requiresAuth: true } },
-  { path: '/deployments', component: Deployments, meta: { requiresAuth: true } },
+
 ];
 
 const router = createRouter({
