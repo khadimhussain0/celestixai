8f28bcc42283a4a8ae53321b93d0465faac890b5
Khadim Hussain <khadimkhan78655@gmail.com>
2024-02-24 17:50:57 +0500

    backend/file naming schema changes

--- celestixai-backend/app/routes/rag.py
+++ celestixai-backend/app/routes/rag.py
@@ -1,12 +1,12 @@
 import os
-import shutil
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.core.auth import get_current_user
 from app.models.dataset import Dataset
-from app.core.config import FILE_STORAGE_PATH, VECTOR_FILE_STORAGE_PATH
+from app.core.config import FILE_STORAGE_PATH
 from app.services.vectorstore import DocumentProcessor
+from langchain_community.embeddings import OllamaEmbeddings
 
 router = APIRouter(
     prefix="/rag",
@@ -24,27 +24,13 @@ async def build_document_vector_store(datasets: dict, current_user=Depends(get_c
     if not user_datasets:
         raise HTTPException(status_code=404, detail="No datasets found for the current user with the provided IDs")
 
-    user_dir = os.path.join(VECTOR_FILE_STORAGE_PATH, str(current_user.id))
-    os.makedirs(user_dir, exist_ok=True)
-
-    source_documents_dir = os.path.join(user_dir, "source_documents")
-    os.makedirs(source_documents_dir, exist_ok=True)
-
-    persist_dir = os.path.join(user_dir, "db")
-    os.makedirs(persist_dir, exist_ok=True)
-
+    ollama_embedder= OllamaEmbeddings(base_url='http://celestixai-ollama-1:11434')
+    rag_files = []
     for dataset in user_datasets:
-        dataset_path = os.path.join(FILE_STORAGE_PATH, dataset.filename + "." + dataset.uuid)
-        destination_path = os.path.join(source_documents_dir, dataset.filename)
+        dataset_path = os.path.join(FILE_STORAGE_PATH, dataset.uuid +"____" + dataset.filename)
         print(dataset_path)
-        print(destination_path)
-        if not os.path.exists(destination_path):
-            try:
-                shutil.copy(dataset_path, destination_path)
-            except FileNotFoundError:
-                raise HTTPException(status_code=500, detail="Failed to copy dataset file")
-    db.close()
-    processor = DocumentProcessor(source_directory=source_documents_dir, persist_directory=persist_dir)
-    await processor.process_documents()
+        rag_files.append(dataset_path)
+    processor = DocumentProcessor(files=rag_files, ollama_embedder=ollama_embedder)
+    await processor.process_documents(str(current_user.id))
 
     return {"success": True}
